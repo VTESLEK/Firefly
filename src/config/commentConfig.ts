72b479ed9d76861558136c98f88b763916f375a4
VTESLEK <xiangchunqinxiang@gmail.com>
2026-07-14 10:54:09 +0800

    评论

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -23,7 +23,7 @@ export const commentConfig: CommentConfig = {
 	//waline评论系统配置
 	waline: {
 		// waline 后端服务地址
-		serverURL: "https://waline-8i0o40i5d-xevon.vercel.app",
+		serverURL: "https://xane78.us.ci",
 		// 设置 Waline 评论系统语言
 		lang: "zh-CN",
 		// 设置 Waline 评论系统表情地址
